0730eac3102713973c5f8feb246440a29a5c1f1d
IMOROU Bonaventure <imoroubonaventure@gmail.com>
2020-02-17 11:26:30 +0100

    Enpowering validation with unique identification

--- register.php
+++ register.php
@@ -1,15 +1,30 @@
 <?php require 'inc/header.php'; ?>
 
 
+
     <?php 
 
          if (!empty($_POST)) {
             $errors = array();
+
+        //Connexion à la base des données
+            require_once('inc/db.php');
+
         //Validation of posted username
 
             if (empty($_POST['username']) || !preg_match('/^[a-z0-9-_]+$/i', $_POST['username'])) {
 
             	$errors['username'] = "Votre pseudo n'est valide";
+            }else{
+                $req = $pdo->prepare("SELECT id FROM users WHERE username = ?");
+                $req->execute(array($_POST['username']));
+
+                $user = $req->fetch();
+
+               if ($user) {
+                   $errors['username'] = "Ce nom existe déjà";
+               }
+
             }
 
         //Validation of posted email
@@ -18,6 +33,15 @@
             if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
          	     
          	     $errors['email'] = "Votre email n'est pas valide";
+            }else{
+                $req = $pdo->prepare("SELECT id FROM users WHERE email = ?");
+                $req->execute(array($_POST['email']));
+
+                $email = $req->fetch();
+
+               if ($email) {
+                   $errors['email'] = "Cet email existe déjà";
+               }
             }
         //Validation de mot de passe
 
@@ -25,10 +49,10 @@
             	$errors['password'] = " Vous devez entrer un mot de passe valide ";
             }
 
+
             if (empty($errors)) {
 
-            //Connexion à la base des données
-                require_once('inc/db.php');
+            
 
             //Préparations et insertions des réquêtes dans la base des données
 
@@ -37,7 +61,7 @@
                 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
                 $req->execute(array($_POST['username'], $_POST['email'], $password)); 
-                
+
                 die('Votre compte a bien été créé'); 
             }
             
@@ -45,7 +69,7 @@
             debug($errors);
 
             
-         }
+        }
 
 
     ?>
